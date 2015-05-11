77cd3012d65f02fb557140c387a3dcafc9083f20
ASE2345 <alexakis1997@hotmail.com>
2015-05-11 23:19:32 +0300

    Update hw.glove.mod.perm.w.on.sh

--- rootdir/hw.glove.mod.perm.w.on.sh
+++ rootdir/hw.glove.mod.perm.w.on.sh
@@ -15,20 +15,16 @@ mount_needed=false;
 
 if [ "$ACTUAL" = "136" ];then
   mount -o rw,remount,barrier=1 /system
-  sed -i 's/^.*echo.*$/echo '$ACTUAL' > '$GLOVEMODENODE'/' /system/etc/init.d/glove_mode_set
+  sed -i 's/^.*echo.*$/echo '$ACTUAL' > '$GLOVEMODENODE'/' /system/bin/glove_mode_set.sh
   mount_needed=true;
 fi
 
 if [ "$ACTUAL" = "0" ];then
   mount -o rw,remount,barrier=1 /system
-  sed -i 's/^.*echo.*$/echo '$ACTUAL' > '$GLOVEMODENODE'/' /system/etc/init.d/glove_mode_set
+  sed -i 's/^.*echo.*$/echo '$ACTUAL' > '$GLOVEMODENODE'/' /system/bin/glove_mode_set.sh
   mount_needed=true;
 fi
 
 if $mount_needed ;then
   mount -o ro,remount,barrier=1 /system
 fi
-
-sleep 10
-chmod 440 /sys/devices/i2c-3/3-0024/main_ttsp_core.cyttsp4_i2c_adapter/signal_disparity
-chmod 440 /sys/devices/i2c-3/3-0024/main_ttsp_core.cyttsp4_i2c_adapter/finger_threshold
