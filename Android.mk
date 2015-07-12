410d48cf45e2deae63e7982bbcf8a6119cc08ca5
Mrcl1450 <marcelo-fil@outlook.com>
2015-07-12 14:13:22 +0100

    wcnss: Fix WLAN driver config file symlink
    WLAN driver config file should be accessed from data partition. This
    change also adds wcnss service, which is started at bootup.
    
    Change-Id: Ic67bc3ce8202fe05bd38a8fb6acd17c7bb31afdd

--- Android.mk
+++ Android.mk
@@ -8,4 +8,10 @@ ifeq ($(TARGET_DEVICE),huashan)
         $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_anc.bin; \
         ln -sf /data/misc/audio/mbhc.bin \
         $(TARGET_OUT_ETC)/firmware/wcd9310/wcd9310_mbhc.bin)
+
+    $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
+        ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
+        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
+        ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+        $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 endif
